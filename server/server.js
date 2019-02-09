348b78d2103ed6a1f687261931334af82c35151b
Milos <epicmilos@gmail.com>
2019-02-09 16:18:12 +0100

    add geolocation support

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');//not buildin
 const socketIO = require('socket.io');//not buildin
 
-const {generateMessage}=require('./utils/message');
+const {generateMessage, generateLocationMessage}=require('./utils/message');
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -37,7 +37,7 @@ socket.broadcast.emit('newMessage',generateMessage('Admin','new user joined the 
 socket.on('createMessage',(message,callback)=>{
     console.log('createMessage', message);
     io.emit('newMessage',generateMessage(message.from,message.text));
-    callback();
+    callback('this is from the server');
     
     // socket.broadcast.emit('newMessage',{
     //     from: message.from,
@@ -45,6 +45,10 @@ socket.on('createMessage',(message,callback)=>{
     //     createdAt: new Date().getTime()
     // });
 
+});
+
+socket.on('createLocationMessage',(coords)=>{
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude,coords.longitude));
 });
 
 socket.on('disconnect', ()=>{
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage}=require('./message');
+var {generateMessage, generateLocationMessage}=require('./message');
 
 
 describe('generatMessage',()=>{
@@ -17,5 +17,23 @@ describe('generatMessage',()=>{
     });
    
 });
+
+    describe('generateLocationMessage',()=>{
+        it('should generate correct location object',()=>{
+            var from = 'AdminTest';
+            var latitude = 10;
+            var longitude = 15;
+            var url = 'https://www.google.com/maps?q=10,15';
+            var message = generateLocationMessage(from, latitude, longitude);
+            // expect(message.from).toBe(from); we can do it with toMatchObject
+            // expect(message.url).toBe(url);
+            // expect(message).toInclude({from,url}); OLD
+            expect(message).toMatchObject({from,url});
+            expect(typeof message.createdAt).toBe('number');
+        
+        });
+    
+    });
+
 
 
